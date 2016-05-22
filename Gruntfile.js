e9d92c08ae18cc2553a2c603cd4607f46d1beddb
critmcdonald <christian@cmcdonald.com>
2016-05-22 00:23:19 -0500

    adding local server for development

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,15 +43,54 @@ module.exports = function(grunt) {
           'dist/styles.css': 'source/stylesheets/main.scss'
         }
       }
-    }
+    },
 
+    // watch locations
+    watch: {
+      options: {
+        livereload: true,
+      },
+      scripts: {
+        files: ['source/javascripts/**.js'],
+        tasks: ['uglify']
+      },
+      styles: {
+        files: ['source/stylesheets/**.scss'],
+        tasks: ['sass']
+      },
+      markup: {
+        files: ['index.html']
+      }
+    },
+
+  // Creating a local server
+    connect: {
+      server: {
+        options: {
+          hostname: 'localhost',
+          base: './',
+          keepalive: true,
+          livereload: true,
+          open: true
+        }
+      }
+    },
+    concurrent: {
+      options: {
+        logConcurrentOutput: true
+      },
+      dev: ['connect', 'watch']
+    }
   });
 
   // Load the task plugins
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-concurrent');
 
-  grunt.registerTask('default', ['clean', 'sass', 'uglify']);
+  grunt.registerTask('default', ['clean', 'sass', 'uglify', 'concurrent']);
 
 };
